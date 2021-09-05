8acbe098f63226050721995d1173036e2cd8bb20
Allen Lin <azflin@users.noreply.github.com>
2021-09-04 21:36:16 -0400

    make wallet table

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,6 +1,5 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useParams } from "react-router-dom";
-import NftTile from "./NftTile";
 
 export default React.memo(function Wallet() {
   const { address } = useParams();
@@ -9,19 +8,31 @@ export default React.memo(function Wallet() {
   useEffect(async () => {
     console.log("Wallet useEffect");
     const response = await fetch(
-      `https://api.paintswap.finance/userNFTs/${address}?allowNSFW=true&numToFetch=20&numToSkip=0`,
+      `https://api.paintswap.finance/userNFTs/${address}?allowNSFW=true&numToFetch=100&numToSkip=0`,
     );
     const data = await response.json();
     setNfts(data.nfts);
-    console.log(data.nfts);
   }, [address]);
 
   return (
-    <>
-      <h1>Displaying NFTs of { address }</h1>
-      {nfts && nfts.map((nft =>
-        <NftTile key={nft.id} tokenURI={nft.nft.uri}></NftTile>
-      ))}
-    </>
+    <div>
+      <h1>{ address.slice(0, 6) + "..." + address.slice(address.length - 4, address.length) }'s NFTs</h1>
+      <table>
+        <thead>
+          <th>ERC721 Contract</th>
+          <th>tokenId</th>
+          <th>tokenURI</th>
+        </thead>
+        <tbody>
+          {nfts.map((nft) =>
+            <tr>
+              <td>{nft.address}</td>
+              <td>{nft.nft.tokenId}</td>
+              <td>{nft.nft.uri}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   )
 });
