57c4f91eed862546b52580208b6c574f5cf7a5c7
Allen Lin <azflin@users.noreply.github.com>
2021-09-04 20:12:21 -0400

    continue wallet

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,10 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import { useParams } from "react-router-dom";
+import NftTile from "./NftTile";
 
-export default function Wallet() {
+export default React.memo(function Wallet() {
   const { address } = useParams();
+  const [nfts, setNfts] = useState();
+
+  useEffect(async () => {
+    console.log("Wallet useEffect");
+    const response = await fetch(
+      `https://api.paintswap.finance/userNFTs/${address}?allowNSFW=true&numToFetch=20&numToSkip=0`,
+    );
+    const data = await response.json();
+    setNfts(data.nfts);
+    console.log(data.nfts);
+  }, [address]);
 
   return (
-    <h1>Wallet Address: { address }</h1>
+    <>
+      <h1>Displaying NFTs of { address }</h1>
+      {nfts && nfts.map((nft =>
+        <NftTile key={nft.id} tokenURI={nft.nft.uri}></NftTile>
+      ))}
+    </>
   )
-}
+});
